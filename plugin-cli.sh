7718cd9974845f9daf8e9f9be0513b826226a72d
Đàm Trung Kiên <105965626+damtrungkien@users.noreply.github.com>
2025-05-30 15:11:53 +0700

    Update plugin-cli.sh

--- plugin-cli.sh
+++ plugin-cli.sh
@@ -46,40 +46,37 @@ install_and_activate_theme() {
 
 while true; do
     # Hiển thị menu chọn lựa
-    echo "0) Thoát"
     PS3="Chọn plugin để cài đặt (Nhấn 0 để Thoát): "
     select plugin_name in "${plugins[@]}"; do
-        case $REPLY in
-            0) echo "Thoát chương trình.";
-               rm -f ${pwdd}/plugin-cli.sh;
-               exit ;;
-            *)
-                case $plugin_name in
-                    "Litespeed cache") install_and_activate_plugin litespeed-cache ;;
-                    "SpeedyCache Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/speedycache-pro.zip" ;;
-                    "Elementor Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/elementor-pro-3.28.2.zip" ;;
-                    "Wordfence") install_and_activate_plugin wordfence ;;
-                    "WPS Hide Login") install_and_activate_plugin wps-hide-login ;;
-                    "Really simple SSL") install_and_activate_plugin really-simple-ssl ;;
-                    "Anti-Malware Security") install_and_activate_plugin gotmls ;;
-                    "Redis cache") install_and_activate_plugin redis-cache ;;
-                    "Better search replace") install_and_activate_plugin better-search-replace ;;
-                    "Query monitor") install_and_activate_plugin query-monitor ;;
-                    "Fastest cache") install_and_activate_plugin wp-fastest-cache ;;
-                    "Solid Security Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/ithemes-security-pro-8.5.6.zip" ;;
-                    "WPMU Dev Dashboard") install_and_activate_plugin "https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_wpmu-dev-dashboard-4.11.26.zip" ;;
-                    "SEO by Rank Math Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seo-by-rank-math-pro.zip" ;;
-                    "Elements Kit") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/elementskit-3.6.9.zip" ;;
-                    "Sitepress Multilingual CMS") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/sitepress-multilingual-cms.4.7.4.zip";;
-                    "Seedprod") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seedprod-coming-soon-pro-5-6.18.16.zip";;
-                    "WP Staging Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/wp-staging-pro.zip";;
-                    "Astra Theme") install_and_activate_theme "https://tainguyenwp.azdigi.com/WordPress-Astra/Theme/astra.4.11.0.zip";;
-                    "Astra Child Theme") install_and_activate_theme "https://tainguyenwp.azdigi.com/WordPress-Astra/Theme/astra-child.zip";;
-                    "Astra Addon Plugin") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-addon-plugin-4.8.11.zip";;
-                    "Astra Premium Sites") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-premium-sites-4.4.19.zip";;
-                    *) echo "Lựa chọn không hợp lệ. Hãy chọn lại." ;;
-                esac
-                ;;
+        if [ "$REPLY" = "0" ]; then
+            echo "Thoát chương trình."
+            rm -f "${pwdd}/plugin-cli.sh"
+            exit
+        fi
+        case $plugin_name in
+            "Litespeed cache") install_and_activate_plugin litespeed-cache ;;
+            "SpeedyCache Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/speedycache-pro.zip" ;;
+            "Elementor Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/elementor-pro-3.28.2.zip" ;;
+            "Wordfence") install_and_activate_plugin wordfence ;;
+            "WPS Hide Login") install_and_activate_plugin wps-hide-login ;;
+            "Really simple SSL") install_and_activate_plugin really-simple-ssl ;;
+            "Anti-Malware Security") install_and_activate_plugin gotmls ;;
+            "Redis cache") install_and_activate_plugin redis-cache ;;
+            "Better search replace") install_and_activate_plugin better-search-replace ;;
+            "Query monitor") install_and_activate_plugin query-monitor ;;
+            "Fastest cache") install_and_activate_plugin wp-fastest-cache ;;
+            "Solid Security Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/ithemes-security-pro-8.5.6.zip" ;;
+            "WPMU Dev Dashboard") install_and_activate_plugin "https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_wpmu-dev-dashboard-4.11.26.zip" ;;
+            "SEO by Rank Math Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seo-by-rank-math-pro.zip" ;;
+            "Elements Kit") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/elementskit-3.6.9.zip" ;;
+            "Sitepress Multilingual CMS") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/sitepress-multilingual-cms.4.7.4.zip";;
+            "Seedprod") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seedprod-coming-soon-pro-5-6.18.16.zip";;
+            "WP Staging Pro") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/wp-staging-pro.zip";;
+            "Astra Theme") install_and_activate_theme "https://tainguyenwp.azdigi.com/WordPress-Astra/Theme/astra.4.11.0.zip";;
+            "Astra Child Theme") install_and_activate_theme "https://tainguyenwp.azdigi.com/WordPress-Astra/Theme/astra-child.zip";;
+            "Astra Addon Plugin") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-addon-plugin-4.8.11.zip";;
+            "Astra Premium Sites") install_and_activate_plugin "https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-premium-sites-4.4.19.zip";;
+            *) echo "Lựa chọn không hợp lệ. Hãy chọn lại." ;;
         esac
         break
     done
